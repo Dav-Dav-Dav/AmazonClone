031fb96131f7cf2743ab23692a8d54630798aee7
Davindar <davdavdav4@gmail.com>
2020-07-16 15:24:35 +0100

    Trash icons removes basket products

--- src/Basket/BasketMap.js
+++ src/Basket/BasketMap.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import "../styles.css";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function BasketMap(props) {
+  const { removeFromCart } = useContext(Context);
+
   return (
     <div className="App">
       <img alt="some value" className="imageSize" src={props.url} />
       <h1>£5.99</h1>
+      <FontAwesomeIcon
+        onClick={() => removeFromCart(props.id)}
+        className="bin"
+        icon={faTrash}
+      />
     </div>
   );
 }
