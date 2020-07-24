022f28e5068fd5a7155a3f673bf5e63da685bacd
Davindar <davdavdav4@gmail.com>
2020-07-24 15:49:12 +0100

    Each order has unique order number

--- src/Context.js
+++ src/Context.js
@@ -7,6 +7,7 @@ function ContextProvider({ children }) {
   const [photos] = useState(data);
   const [cart, setCart] = useState([]);
   const [history, setHistory] = useState([]);
+  const [orderNumber, setOrderNumer] = useState([1]);
 
   function addToCart(x) {
     setCart(cart.concat(x));
@@ -17,18 +18,18 @@ function ContextProvider({ children }) {
   }
 
   function saveOrder(x, totalPrice) {
-    console.log(totalPrice);
+    setOrderNumer(
+      orderNumber.concat([orderNumber[orderNumber.length - 1] + 1])
+    );
     let a = new Map([
       ["date", new Date()],
-      ["orderNumber", 5],
+      ["orderNumber", orderNumber[orderNumber.length - 1]],
       ["totalPrice", totalPrice]
     ]);
     const obj = Object.fromEntries(a);
     setHistory(history.concat([[obj, x]]));
     setCart([]);
   }
-
-  console.log(history);
 
   //console.log( history )
 
