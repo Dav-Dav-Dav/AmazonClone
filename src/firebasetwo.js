bd6deea89ca74190b013952df515eb29435309d6
Dav <davdavdav4@gmail.com>
2020-08-01 18:21:31 +0100

    Firebase login working

---
+++ src/firebasetwo.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+// import "firebase/database"
+// import "firebase/auth"
+
+const config = {
+  apiKey: "AIzaSyAaXvg-kSp0wGTXh4cJ4Xzs6cuak18stuk",
+  authDomain: "clone-8f0bc.firebaseapp.com",
+  databaseURL: "https://clone-8f0bc.firebaseio.com",
+  projectId: "clone-8f0bc",
+  storageBucket: "clone-8f0bc.appspot.com",
+  messagingSenderId: "446578394849",
+  appId: "1:446578394849:web:e7c592b34e35489d7159a0"
+};
+
+// firebase.initializeApp(config)
+
+// const app = firebase.initializeApp(config)
+// const db = app.database()
+
+// export { app }
+// export { db }
+
+export default (!firebase.apps.length
+  ? firebase.initializeApp(config)
+  : firebase.app());
