5b37bc8ec330eb02cdc4758d0f6fbe44e42f1bae
Dav <davdavdav4@gmail.com>
2020-08-08 17:43:36 +0100

    Navigation bug fix

--- src/Pages/Navigation.js
+++ src/Pages/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css";
+import "../styles.css";
 import { Link } from "react-router-dom";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
