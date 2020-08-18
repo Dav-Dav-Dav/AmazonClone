b4fb5fa4016c1722d0015d0a7ae0244f8f7d7095
Dav <davdavdav4@gmail.com>
2020-08-18 09:43:17 +0100

    Top section separated into box's (left right))

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,17 +1,33 @@
 import React from "react";
 import styled from 'styled-components';
 
-const Paragraph = styled.p`
-  font-size: 40px;
-  line-height: 18px;
-  color: red;
-  margin-bottom: 20px;
+const TwoBoxes = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 953.74px;
+	  gap: 15px 10px;
+
+  .right {
+    grid-area: 1 / 1 / 1 / 1;
+    background: yellow;
+  }
+
+  .left {
+    background: green;
+    grid-area: 1 / 2 / 1 / 2;
+    background: green;
+  }
 `;
+
 
 export default function Homepage() {
   return (
-    <div className="App">
-      <Paragraph>Homepage</Paragraph>
-    </div>
+    <TwoBoxes>
+        <div className="right"> 
+          <p>Homepage</p>
+        </div>
+        <div className="left"> 
+        </div> 
+    </TwoBoxes>
   );
 }
