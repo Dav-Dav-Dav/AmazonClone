c000a68d30f51fc11b70b841f8871c1a39110afc
Dav <davdavdav4@gmail.com>
2020-08-18 10:54:23 +0100

    Text positioned properly - box's

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,16 +1,15 @@
 import React from "react";
-import styled from 'styled-components';
-import greyPot from '../Images/greyPlantPot.jpg';
+import styled from "styled-components";
+import greyPot from "../Images/greyPlantPot.jpg";
 
 const TwoBoxes = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: 953.74px;
-	  gap: 15px 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 953.74px;
+  gap: 15px 10px;
 
   .right {
     grid-area: 1 / 1 / 1 / 1;
-    background: yellow;
   }
 
   .left {
@@ -25,16 +24,40 @@ const TwoBoxes = styled.div`
   }
 `;
 
+const TwoBoxesText = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+
+  .twoBoxPosition {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 export default function Homepage() {
   return (
     <TwoBoxes>
-        <div className="right"> 
-          <p>Homepage</p>
-        </div>
-        <div className="left"> 
-          <img src={ greyPot } />
-        </div> 
+      <div className="right">
+        <TwoBoxesText>
+          <div className="twoBoxPosition">
+            <p>
+              Plants <br /> Delivered
+            </p>
+            <p>
+              Patch helps you discover the best plants for your space, <br />{" "}
+              delivers them to your door and helps you look after them.
+            </p>
+            <button>PLANTS</button>
+            <button>POTS</button>
+          </div>
+        </TwoBoxesText>
+      </div>
+      <div className="left">
+        <img src={greyPot} />
+      </div>
     </TwoBoxes>
   );
 }
